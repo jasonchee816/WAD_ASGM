71fa67f45ccd48935d711fdd53941b8b3f31f032
Yang Chu Yan <chuyanyang01@gmail.com>
2022-08-22 22:45:47 +0800

    Revert "Merge branch 'master' of https://github.com/jasonchee816/WAD_ASGM"
    This reverts commit 257a7afbe0cb4e298b394c564c10bd7ed699dfdb, reversing
    changes made to ac492446d131c55089be10569e14a88bfc6f7359.

--- dataStore/database.py
+++ dataStore/database.py
@@ -9,7 +9,7 @@ db.execute('''CREATE TABLE MenuData(
     name text NOT NULL,
     desc text NOT NULL,
     price REAL NOT NULL,
-    image text NOT NULL
+    image BLOB NOT NULL
 )''')
 
 cursor = db.cursor()
@@ -51,103 +51,107 @@ VALUES(9, "pizza", "Chicken Supreme", "Pizza with tomato sauce, chicken meat, ch
 30.00, "../images/chickenSupreme.png")''')
 
 cursor.execute(''' INSERT INTO MenuData(id, category, name, desc, price, image) 
-VALUES(10, "pizza", "Blazing Seafood", "Pizza with spicy sweet sour sauce, tuna, crabsticks, pineapples, capsicums, onions, mozzarella cheese.",
+VALUES(10, "pizza", "Chicken Supreme", "Pizza with tomato sauce, chicken meat, chicken salami, chicken loaf, mushrooms, capsicums, onions, mozzarella cheese, tomatoes.",
+30.00, "../images/chickenSupreme.png")''')
+
+cursor.execute(''' INSERT INTO MenuData(id, category, name, desc, price, image) 
+VALUES(11, "pizza", "Blazing Seafood", "Pizza with spicy sweet sour sauce, tuna, crabsticks, pineapples, capsicums, onions, mozzarella cheese.",
 30.00, "../images/blazingSeafood.png")''')
 
 cursor.execute(''' INSERT INTO MenuData(id, category, name, desc, price, image) 
-VALUES(11, "pizza", "Triple Chicken", "Pizza with thousand island sauce, chicken rolls, chicken meat, chicken salami, mushrooms, tomatoes, onions, mozzarella cheese.",
+VALUES(12, "pizza", "triple Chicken", "Pizza with thousand island sauce, chicken rolls, chicken meat, chicken salami, mushrooms, tomatoes, onions, mozzarella cheese.",
 30.00, "../images/tripleChicken.png")''')
 
 cursor.execute(''' INSERT INTO MenuData(id, category, name, desc, price, image) 
-VALUES(12, "pasta", "Baked Feta Pasta", "Cherry tomatoes and a whole block of feta are baked until soft and melty, then tossed with garlic, basil and cooked noodles for a beautifully cheesy dish.",
+VALUES(13, "pasta", "Baked Feta Pasta", "Cherry tomatoes and a whole block of feta are baked until soft and melty, then tossed with garlic, basil and cooked noodles for a beautifully cheesy dish.",
 15.00, "../images/bakedFetaPasta.png")''')
 
 cursor.execute(''' INSERT INTO MenuData(id, category, name, desc, price, image) 
-VALUES(13, "pasta", "Pasta with Garlic and Cheese", "Kind of like a deconstructed pesto, using parsley instead of basil.",
+VALUES(14, "pasta", "Pasta with Garlic and Cheese", "Kind of like a deconstructed pesto, using parsley instead of basil.",
 16.70, "../images/pastaGarlicCheese.png")''')
 
 cursor.execute(''' INSERT INTO MenuData(id, category, name, desc, price, image) 
-VALUES(14, "pasta", "Pasta with Fresh Tomato Sauce", "Romas (aka plum tomatoes) fit the bill, but there are plenty of heirlooms with a similar flesh-to-seed ratio.",
+VALUES(15, "pasta", "Pasta with Fresh Tomato Sauce", "Romas (aka plum tomatoes) fit the bill, but there are plenty of heirlooms with a similar flesh-to-seed ratio.",
 20.15, "../images/pastaTomatoSauce.png")''')
 
 cursor.execute(''' INSERT INTO MenuData(id, category, name, desc, price, image) 
-VALUES(15, "burger", "Whooper Jr", "A mouth-watering, flame-grilled beef patty topped with fresh tomatoes, crisp lettuce, tangy pickles and crunchy onions, served on a toasted sesame seed bun and flavored with creamy mayo and ketchup.",
+VALUES(16, "burger", "Whooper Jr", "A mouth-watering, flame-grilled beef patty topped with fresh tomatoes, crisp lettuce, tangy pickles and crunchy onions, served on a toasted sesame seed bun and flavored with creamy mayo and ketchup.",
 18.00, "../images/whooperJr.png")''')
 
 cursor.execute(''' INSERT INTO MenuData(id, category, name, desc, price, image) 
-VALUES(16, "burger", "Long Cheesy Onion Beef", "Featuring 2 pieces of our signature flame-grilled beef patties topped with Nachos Cheese sauce, mayonnaise, mustard, pickles and fresh onions served between two long sesame seed buns.",
+VALUES(17, "burger", "Long Cheesy Onion Beef", "Featuring 2 pieces of our signature flame-grilled beef patties topped with Nachos Cheese sauce, mayonnaise, mustard, pickles and fresh onions served between two long sesame seed buns.",
 22.00, "../images/cheesyOnionBeef.png")''')
 
 cursor.execute(''' INSERT INTO MenuData(id, category, name, desc, price, image) 
-VALUES(17, "burger", "Double Cheeseburger", "Make room for our Double Cheeseburger, two signature flame-grilled beef patties topped with a simple layer of melted American cheese, crinkle cut pickles, yellow mustard, and ketchup on a toasted sesame seed bun.",
+VALUES(18, "burger", "Double Cheeseburger", "Make room for our Double Cheeseburger, two signature flame-grilled beef patties topped with a simple layer of melted American cheese, crinkle cut pickles, yellow mustard, and ketchup on a toasted sesame seed bun.",
 26.00, "../images/doubleCheeseburger.png")''')
 
 cursor.execute(''' INSERT INTO MenuData(id, category, name, desc, price, image)  
-VALUES(18, "coffeeTea", "Caffe Latte", "Espresso topped with Velvety Smooth Steamed Milk.",
+VALUES(19, "coffeeTea", "Caffe Latte", "Espresso topped with Velvety Smooth Steamed Milk.",
 12.00, "../images/latte.png")''')
 
 cursor.execute(''' INSERT INTO MenuData(id, category, name, desc, price, image)  
-VALUES(19, "coffeeTea", "Mocha", "An Indulgent Blend of Espresso & Hot Chocolate topped with Velvety Steamed Milk.",
+VALUES(20, "coffeeTea", "Mocha", "An Indulgent Blend of Espresso & Hot Chocolate topped with Velvety Steamed Milk.",
 10.00, "../images/mocha.png")''')
 
 cursor.execute(''' INSERT INTO MenuData(id, category, name, desc, price, image)  
-VALUES(20, "coffeeTea", "Cappuccino", "Espresso with Smooth Steamed Milk & finished with a Light Drizzle of Chocolate.",
+VALUES(21, "coffeeTea", "Cappuccino", "Espresso with Smooth Steamed Milk & finished with a Light Drizzle of Chocolate.",
  12.00, "../images/cappuccino.png")''')
 
 cursor.execute(''' INSERT INTO MenuData(id, category, name, desc, price, image) 
-VALUES(21, "coffeeTea", "Butterfly Pea Tea", "An Earthy, Woody Tea with an Exquisite Blue Tinge and Distinct Floral Aroma.",
+VALUES(22, "coffeeTea", "Butterfly Pea Tea", "An Earthy, Woody Tea with an Exquisite Blue Tinge and Distinct Floral Aroma.",
 9.00, "../images/butterflyPeaTea.png")''')
 
 cursor.execute(''' INSERT INTO MenuData(id, category, name, desc, price, image)  
-VALUES(22, "coffeeTea", "Lemongrass Ginger Tea", "Fragrant and Aromatic, this is refreshing lemongrass-flavoured tea with a slight trace of ginger.",
+VALUES(23, "coffeeTea", "Lemongrass Ginger Tea", "Fragrant and Aromatic, this is refreshing lemongrass-flavoured tea with a slight trace of ginger.",
 9.00, "../images/lemongrassGinger.png")''')
 
 cursor.execute(''' INSERT INTO MenuData(id, category, name, desc, price, image) 
-VALUES(23, "fruitJuice", "Fig Butterfly", "Fig juice with an exquisite pink tinge.",
+VALUES(24, "fruitJuice", "Fig Butterfly", "Fig juice with an exquisite pink tinge.",
 11.00, "../images/figButterfly.png")''')
 
 cursor.execute(''' INSERT INTO MenuData(id, category, name, desc, price, image)  
-VALUES(24, "fruitJuice", "Mango Tango", "A rich mixture of mango, passion fruit and apple.",
+VALUES(25, "fruitJuice", "Mango Tango", "A rich mixture of mango, passion fruit and apple.",
 10.00, "../images/mangoTango.png")''')
 
 cursor.execute(''' INSERT INTO MenuData(id, category, name, desc, price, image)  
-VALUES(25, "fruitJuice", "Golden Passion", "Sweet pineapple, passion fruit, guava with a touch of mint leaves",
+VALUES(26, "fruitJuice", "Golden Passion", "Sweet pineapple, passion fruit, guava with a touch of mint leaves",
 13.00, "../images/goldenPassion.png")''')
 
 cursor.execute(''' INSERT INTO MenuData(id, category, name, desc, price, image)  
-VALUES(26, "fruitJuice", "Pink Lady", "Dragon Fruit, Pear, Grapes, topped with mint leaf.",
+VALUES(27, "fruitJuice", "Pink Lady", "Dragon Fruit, Pear, Grapes, topped with mint leaf.",
 10.00, "../images/pinkLady.png")''')
 
 cursor.execute(''' INSERT INTO MenuData(id, category, name, desc, price, image)  
-VALUES(27, "fruitJuice", "Greenty Minty", "A mix of green apple and kiwi with mint leaf and lime.",
+VALUES(28, "fruitJuice", "Greenty Minty", "A mix of green apple and kiwi with mint leaf and lime.",
 10.00, "../images/greentyMinty.png")''')
 
 cursor.execute(''' INSERT INTO MenuData(id, category, name, desc, price, image) 
-VALUES(28, "smoothies", "Chocolate Smoothie", "Sweet and Chocolaty, a Perfect Drink for your Date.",
+VALUES(29, "smoothies", "Chocolate Smoothie", "Sweet and Chocolaty, a Perfect Drink for your Date.",
 13.00, "../images/chocolateSmoothie.jpg")''')
 
 cursor.execute(''' INSERT INTO MenuData(id, category, name, desc, price, image) 
-VALUES(29, "smoothies", "Dragon Fruit Smoothies", "Made with Fresh Dragon Fruit, Tasty and Hydrating!",
+VALUES(30, "smoothies", "Dragon Fruit Smoothies", "Made with Fresh Dragon Fruit, Tasty and Hydrating!",
 13.00, "../images/dragonFruitSmoothie.jpg")''')
 
 cursor.execute(''' INSERT INTO MenuData(id, category, name, desc, price, image) 
-VALUES(30, "smoothies", "Banana Smoothie", "Healthy and Delicious! Nothing can go wrong with a Banana Smoothie!",
+VALUES(31, "smoothies", "Banana Smoothie", "Healthy and Delicious! Nothing can go wrong with a Banana Smoothie!",
 13.00, "../images/bananaSmoothie.jpg")''')
 
 cursor.execute(''' INSERT INTO MenuData(id, category, name, desc, price, image)  
-VALUES(31, "alcohol", "Penfolds St.Henri Shiraz 2016", "Variety: Syrah/Shiraz\nVintage: 2016\nAppellation: Barossa Valley, Australia",
+VALUES(32, "alcohol", "Penfolds St.Henri Shiraz 2016", "Variety: Syrah/Shiraz\nVintage: 2016\nAppellation: Barossa Valley, Australia",
 338.00, "../images/bollinger.jpg")''')
 
 cursor.execute(''' INSERT INTO MenuData(id, category, name, desc, price, image) 
-VALUES(32, "alcohol", "Taittinger Reserve Brut NV Champagne", "Variety: France\nVintage: 2020\nAppellation: Reims",
+VALUES(33, "alcohol", "Taittinger Reserve Brut NV Champagne", "Variety: France\nVintage: 2020\nAppellation: Reims",
  338.00, "../images/penfolds2016.png")''')
 
 cursor.execute(''' INSERT INTO MenuData(id, category, name, desc, price, image) 
-VALUES(33, "dessert", "Cheesecake", "A creamy, sweet, and delicious no bake berry cheesecake.",
+VALUES(34, "dessert", "Cheesecake", "A creamy, sweet, and delicious no bake berry cheesecake.",
 8.00, "../images/cheeseCake.jpg")''')
 
 cursor.execute(''' INSERT INTO MenuData(id, category, name, desc, price, image)  
-VALUES(34, "dessert", "Snickers Pie", "Oreo crust, silky and airy chocolate filling and finished with chopped candy.",
+VALUES(35, "dessert", "Snickers Pie", "Oreo crust, silky and airy chocolate filling and finished with chopped candy.",
 12.00, "../images/snickersPie.jpg")''')
 
 
