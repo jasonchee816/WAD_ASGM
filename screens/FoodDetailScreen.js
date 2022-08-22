65e61180fbc7fb37212cdd2ce8409166fb1234bf
jasonchee816 <jasonchee01@hotmail.com>
2022-08-22 12:09:37 +0800

    Test MenuData with FoodDetailScreen

---
+++ screens/FoodDetailScreen.js
@@ -0,0 +1,18 @@
+import react, {Component} from 'react';
+import {View, Text} from 'react-native';
+
+// let foodData = require('../data/food')
+
+export default class FoodDetailScreen extends Component<Props> {
+  render() {
+    let food = this.props.params.food;
+    let foodName = foods[food].foodName;
+    let foodDesc = foods[food].foodDesc;
+    let foodPrice = foods[food].foodPrice;
+    this.props.navigation.setOptions({
+      title: foodName,
+    });
+
+    return <View></View>;
+  }
+}
